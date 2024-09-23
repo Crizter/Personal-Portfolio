17bbcaa4c00acd603d6028c0c0bfc1869301fd79
Harsh Sharma <harshsharma90866@gmail.com>
2024-09-23 19:51:55 +0530

    Personal portfolio

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import './App.css'
+import { Routes, Route, useLocation } from 'react-router'
+import About from './components/About' 
+import Home from './components/Home'
+import Contact from './components/Contact'
+import Navbar from './components/Navbar'
+import ModelViewer from './components/ModelViewer'
+
+
+
+function App() {
+
+  const location = useLocation() ; 
+  return (
+    <>
+      <Navbar />
+      <Routes location = {location} key= {location.pathname}>
+          <Route index element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about-us" element={<About />} />
+          <Route path="/projects" element={<About />} />
+          <Route path = "/models" element={<ModelViewer />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App
